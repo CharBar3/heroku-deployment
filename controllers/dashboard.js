39221794c4a4193ad6985fa410ff418866c34e8a
Charles Bartlett <ownageunit@gmail.com>
2022-05-30 20:11:43 -0400

    working on edit route. nested data is hard

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -7,9 +7,66 @@ const Router = express.Router()
 // Index
 Router.get('/', (req, res) => {
     Workout.find({}, (error, allWorkouts) => {
-        // console.log(allWorkouts)
         res.render('dashboard/index.ejs', {workouts: allWorkouts})
     })
+})
+
+// Update
+Router.patch('/:id', (req, res) => {
+
+    
+
+    Workout.findByIdAndUpdate(
+        req.params.id, 
+        req.body, 
+        { new: true }, 
+        (err, foundWorkoutDay) => {
+            console.log(req.body)
+            // console.log(req.body.exercises[0].sets[0].reps)
+        
+        console.log(foundWorkoutDay.exercises[0].name)
+        console.log(foundWorkoutDay.exercises[0].sets[0].reps)
+        foundWorkoutDay.exercises[0].sets[0].reps
+
+        res.redirect(`/dashboard/${req.params.id}`)
+    })
+
+
+
+    
+    // Workout.findById(req.params.id, (err, foundWorkoutDay) => {
+    //     const exerciseNames = Object.keys(req.body)
+    //     // console.log(exerciseNames) 
+    //     const exercises = []
+    //     for (let index = 0; index < exerciseNames.length; index++) {
+    //         const key = exerciseNames[index];
+    //         if (key.startsWith('Exercise')) {
+    //             const splitKey = key.split('-')
+    //             console.log(splitKey, req.body[key])
+    //             if (splitKey.length === 2) {
+    //                 const exercise = {
+    //                     name: req.body[key],
+    //                     sets: []
+    //                 }
+    //                 exercises.push(exercise)
+    //             } else { 
+    //                 exercises.find()
+    //                 if (splitKey[4] === 'Reps') {
+                            
+    //                     }
+    //             }
+
+    //         }
+    //     }
+    //     console.log(exercises)
+    //     const emptyObj = {
+    //         ...foundWorkoutDay,
+    //         title: req.body.title
+            
+    //     }
+    // })
+    
+    
 })
 
 // Edit
@@ -25,7 +82,6 @@ Router.get('/:id/edit', (req, res) => {
 // Show
 Router.get('/:id', (req, res) => {
     Workout.findById(req.params.id, (err, foundWorkoutDay) => {
-        // console.log(foundWorkoutDay) 
         res.render('./dashboard/show.ejs', {
             workoutDay: foundWorkoutDay
         })
--- models/seedWorkouts.js
+++ models/seedWorkouts.js
@@ -3,14 +3,12 @@ module.exports = [
         title: 'Back Day',
         exercises: [{
             name: 'shoulder press',
-            // sets are tracked by array.length
-            // reps and weight are objects inside the array
-            setsRepsWeights: [{reps: 10, weight: '100lbs'}, {reps: 10, weight: '100lbs'}, {reps: 10, weight: '100lbs'}],
+            sets: [{reps: 10, weight: '100lbs'}, {reps: 10, weight: '100lbs'}, {reps: 10, weight: '100lbs'}],
             notes: 'fealt strong the whole way through'
         },
         {
             name: 'bent over barbell row',
-            setsRepsWeights: [{reps: 5, weight: '50lbs'}, {reps: 4, weight: '50lbs'}, {reps: 3, weight: '50lbs'}],
+            sets: [{reps: 5, weight: '50lbs'}, {reps: 4, weight: '50lbs'}, {reps: 3, weight: '50lbs'}],
             notes: 'could not finish strong on the last set'
         }
         ],
@@ -22,15 +20,16 @@ module.exports = [
             name: 'leg press',
             // sets are tracked by array.length
             // reps and weight are objects inside the array
-            setsRepsWeights: [{reps: 10, weight: '100lbs'}, {reps: 10, weight: '100lbs'}, {reps: 10, weight: '100lbs'}],
+            sets: [{reps: 10, weight: '100lbs'}, {reps: 10, weight: '100lbs'}, {reps: 10, weight: '100lbs'}],
             notes: 'fealt strong the whole way through'
         },
         {
             name: 'squat',
-            setsRepsWeights: [{reps: 5, weight: '50lbs'}, {reps: 4, weight: '50lbs'}, {reps: 3, weight: '50lbs'}, {reps: 6, weight: '40lbs'}],
+            sets: [{reps: 5, weight: '50lbs'}, {reps: 4, weight: '50lbs'}, {reps: 3, weight: '50lbs'}, {reps: 6, weight: '40lbs'}],
             notes: 'could not finish strong on the last set'
         }
         ],
         date: '7/8/2022'
     },
+    
 ]
