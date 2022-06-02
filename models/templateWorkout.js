539ab0e59f4b5d02d4df490511ee07de17b5df70
Charles Bartlett <ownageunit@gmail.com>
2022-06-01 22:19:22 -0400

    update edit title and time button, small changes seed data

--- models/templateWorkout.js
+++ models/templateWorkout.js
@@ -1,13 +1,13 @@
 module.exports = 
     {
-        title: 'example title',
+        title: 'Workout Title',
         exercises: [{
-            name: 'exercise 1',
+            name: 'Exercise 1',
             sets: [{reps: 10, weight: '100lbs'}, {reps: 10, weight: '100lbs'}, {reps: 10, weight: '100lbs'}],
             notes: 'notes'
         },
         {
-            name: 'exercise 2',
+            name: 'Exercise 2',
             sets: [{reps: 5, weight: '50lbs'}, {reps: 5, weight: '50lbs'}, {reps: 5, weight: '50lbs'}],
             notes: 'notes'
         }
